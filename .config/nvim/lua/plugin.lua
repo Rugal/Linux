87e420f53458acd2ecc50b0f45da2ab162aee79c
Rugal Bernstein <Rugal@users.noreply.github.com>
2022-07-11 12:10:59 +0800

    Update plugin.lua

--- .config/nvim/lua/plugin.lua
+++ .config/nvim/lua/plugin.lua
@@ -359,9 +359,9 @@ return packer.startup(function(use)
     config = function()
       require('gitsigns').setup {
         signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
-        numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
-        linehl     = true, -- Toggle with `:Gitsigns toggle_linehl`
-        word_diff  = true, -- Toggle with `:Gitsigns toggle_word_diff`
+        numhl      = true, -- Toggle with `:Gitsigns toggle_numhl`
+        linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
+        word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
         attach_to_untracked = true,
         current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
       }
